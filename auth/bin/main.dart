0af679e4812f289501c5607b217c7af4e09c0089
todaelapps <todaelapps@gmail.com>
2022-07-02 14:24:13 +0200

    update_20220702_01

--- auth/bin/main.dart
+++ auth/bin/main.dart
@@ -4,8 +4,7 @@ import 'package:auth/auth.dart';
 import 'package:conduit/conduit.dart';
 
 void main(List<String> arguments) async {
-  final int port = int.parse(Platform.environment["PORT"]??"5432");
-  final service = Application<AppService>()..options.port=port;
+  final int port = int.parse(Platform.environment["PORT"] ?? "8080");
+  final service = Application<AppService>()..options.port = port;
   await service.start(numberOfInstances: 3, consoleLogging: true);
 }
-
